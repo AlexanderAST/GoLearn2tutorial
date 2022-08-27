a4b47accc616f60a9c3bcfc69c9626af9c5b3623
AlexanderAST <89164909109@mail.ru>
2022-08-27 13:05:01 +0300

    3 tut

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// type class struct{
+// 	X int
+// 	Y int
+// }
+// func(c *class) methood(){
+
+// }
 
 func main() {
-	fmt.Println("Hello world!")
+	var b []int
+
+	letters := []string{"aad a", "asfdsf", "adsad"}
+	letters[0] = "4"
+	letters = append(letters, "dodo")
+	fmt.Println(letters)
 }
+
+// func arrays()  {
+// 	var a [2] string
+// 	a[0]="privet"
+// 	a[1]="mir"
+//     fmt.Println(a)
+
+// 	numbers:=[...]int{1,3,6,7,8,8}
+
+// }
+
+// func strut()  {
+// 	p1:=class{
+// 		X: 10,
+// 		Y: 20,
+// 	}
+// 	fmt.Println(p1)
+// }
+// func aboba() {
+// 	a := "Gogag"
+// 	b := 132
+// 	fmt.Println(a)
+// 	fmt.Println(b)
+
+// 	p := &a
+// 	fmt.Println(*p)
+
+// }
